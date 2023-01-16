c7a3035db7b3db4a11a3e3c2f21badb6fe39c96a
Edwin Osorio <edwinosoriogaviria@hotmail.com>
2023-01-16 16:07:50 -0500

    Added tabs component

--- Tabs.tsx
+++ Tabs.tsx
@@ -1,16 +1,47 @@
-import { Tab, Tabs } from '@mui/material';
+import { styled, Tab, Tabs } from '@mui/material';
+import style from './Tabs.module.css';
 
 interface TabsHNProps {
-  tabItems: Array<Object>;
+  tabItems: Array<TabHNProps>;
+  value: number | boolean;
+  onChange: (event: React.SyntheticEvent, value: number) => void;
+  variant?: 'fullWidth' | 'scrollable' | 'standard';
 }
 
-const TabsHN = ({ tabItems }: TabsHNProps) => (
-  <Tabs
-  // classes={{ indicator: 'background-color: rgba(255, 255, 255, 0.555) ' }}
-  >
-    {tabItems.map((item) => (
-      <Tab key={item.id} label={item.label}></Tab>
-    ))}
-  </Tabs>
+interface TabHNProps {
+  key: number;
+  label: string;
+}
+
+const StyledTabs = styled(Tabs)<any>(({ theme }) => ({
+  minHeight: '0',
+  height: '40px',
+  '& .Mui-selected': {
+    backgroundColor: 'var( --primary-color-theme)',
+    borderTopLeftRadius: '5px',
+    borderTopRightRadius: '5px'
+  },
+  '& .MuiButtonBase-root.MuiTab-root.Mui-selected': {
+    color: '#fff'
+  },
+  '& .MuiTab-root:not(.Mui-selected):hover': {
+    backgroundColor: '#eee',
+    borderTopLeftRadius: '5px',
+    borderTopRightRadius: '5px'
+  },
+  '& .MuiButtonBase-root.MuiTab-root': {
+    fontWeight: '600',
+    color: 'var( --text-color-secondary)'
+  }
+}));
+
+const TabsHN = ({ tabItems, value, onChange }: TabsHNProps) => (
+  <div className={style.tabsContainer}>
+    <StyledTabs value={value} onChange={onChange} variant={'fullWidth'}>
+      {tabItems.map((item) => (
+        <Tab key={item.key} label={item.label}></Tab>
+      ))}
+    </StyledTabs>
+  </div>
 );
 export default TabsHN;
