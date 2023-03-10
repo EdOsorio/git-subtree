ebc0c3e5cb6a75b0a25d523f8bb601a6100675ef
Sebastián Buitrago Toro <sebas0811buitrago@gmail.com>
2023-03-10 12:54:04 -0500

    add bullets to favorites to tell between vocabs and domains

--- Bullet.tsx
+++ Bullet.tsx
@@ -1,18 +1,19 @@
 import { Box } from '@mui/material';
 import { BoxProps } from '@mui/material';
 
-const Bullet = (props: BoxProps) => {
-  const { sx = {}, ...rest } = props;
+const Bullet = (props: BoxProps & { size?: string }) => {
+  const { sx = {}, size = '11px', ...rest } = props;
   return (
     <Box
       sx={{
         ...{
-          width: '15px',
-          height: '15px',
+          width: size,
+          height: size,
           bgcolor: 'success.main',
-          borderRadius: '50%',
-          border: '1px solid #DDDBE3',
-          boxShadow: '0px 4px 4px 0px #00000040'
+          borderRadius: '50%'
+          // border: '1px solid #DDDBE3',
+          // boxShadow: '0px 4px 4px 0px #00000040',
+          // opacity: 0.8
         },
         ...sx
       }}
